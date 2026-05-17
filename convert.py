67f29bfc96f09726539c12fcd40c3c4cbfa7c812
NguyenTan-design <96558494+NguyenTan-design@users.noreply.github.com>
2026-05-17 21:45:19 +0700

    F

---
+++ convert.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import json
+
+# Đọc Excel
+df = pd.read_excel(r"C:\Users\quoct\OneDrive - Fuji Machine Asia Pte Ltd\DOCUMENT CONTROL.xlsx")
+
+# Chỉ lấy các cột cần
+df = df[["Category", "TYPE", "Release date", "Title", "LINK"]]
+
+# Format ngày
+df["Release date"] = df["Release date"].dt.strftime("%m-%d-%Y")
+
+# Convert JSON
+data = json.loads(df.to_json(orient="records", force_ascii=False))
+# Xuất JSON
+with open("data.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
+
+print("DONE")
